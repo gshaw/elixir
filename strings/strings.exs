7dda18df248e9c29799512cca30751d5777a7a05
Gerry Shaw <gshaw@venturemedia.com>
2016-02-13 22:24:23 -0800

    Center list of strings

--- strings/strings.exs
+++ strings/strings.exs
@@ -68,4 +68,28 @@ defmodule Strings do
   defp _number_digits(str, value) do
     { value, str }
   end
+
+
+  # Exercise: StringsAndBinaries-5
+  #
+  # Write a function that takes a list of dqs and prints each on a separate
+  # line, centered in a column that has the width of the longest string. Make
+  # sure it works with UTF characters.
+  #
+  # | ​iex>​ center(["cat", "zebra", "elephant"])
+  # |   cat
+  # |  zebra
+  # | elephant
+  #
+  def center(list) do
+    max_length = list
+    |> Enum.map(&(String.length/1))
+    |> Enum.max
+
+    for word <- list do
+      length = String.length(word)
+      padding = round((max_length - length - 1) / 2)
+      IO.puts String.rjust(word, padding + length)
+    end
+  end
 end
